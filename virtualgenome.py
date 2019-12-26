ee2826732bafe76af8719f2ac191a50ab5ca744d
adf-ncgr <adf@ncgr.org>
2019-12-26 08:24:06 -0700

    switch to using the VirtualFile strategy to support buffered reads across individual stream boundaries as some clients (IGV) seem to want to do

--- virtualgenome.py
+++ virtualgenome.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 virtual_fai = None
 virtual_gzi = None
 genome_sizes = {}
+#for VirtualFile handoff strategy
+genome_succession = {}
 from intervaltree import IntervalTree
 virtual_fai_offsets = IntervalTree()
 virtual_gzi_offsets = IntervalTree()
@@ -15,6 +17,44 @@ virtual_gzi_offsets = IntervalTree()
 import re
 byterange_matcher = re.compile('bytes=(\d+)-(\d+)')
 protocol_matcher = re.compile('^https?://')
+
+from io import BufferedReader
+
+class VirtualFile(BufferedReader):
+
+    current_gnm = None
+    current_f = None
+
+    def __init__(self,current_gnm,first_seek,size):
+        self.current_gnm=current_gnm
+        self.current_f = self.open_local_or_remote_gnm(current_gnm,first_seek,size)
+        #super(VirtualFile, self).__init__(self.current_f)
+
+    def read(self,size):
+        import sys
+        sys.stderr.write('read called with ' + self.current_gnm + ' with size = ' + str(size) + '\n')
+        #current_f = self.open_local_or_remote_gnm(self.current_gnm,self.first_seek)
+        b = self.current_f.read(size)
+        while len(b) < size:
+            self.current_f.close()
+            if self.current_gnm in genome_succession:
+                self.current_gnm = genome_succession[self.current_gnm]
+                self.current_f = self.open_local_or_remote_gnm(self.current_gnm,0,size)
+                b += self.current_f.read(size-len(b))
+            else:
+                break
+        return b
+
+    def open_local_or_remote_gnm(self,gnm,seek_to,size):
+        if protocol_matcher.match(gnm):
+            return urllib2.urlopen(urllib2.Request(gnm, headers = {'Range': 'bytes='+str(seek_to)+'-'+str(seek_to+size-1)}))
+        else:
+            import io
+            f = io.open(gnm,'rb')
+            import sys
+            sys.stderr.write('seeking to ' + str(seek_to) + '\n')
+            f.seek(seek_to)
+            return f
 
 @app.route('/virtualgenome.fa.fai')
 @app.route('/virtualgenome.fa.gz.fai')
@@ -55,15 +95,19 @@ def get_fai():
     if not virtual_fai:
         virtual_fai = create_virtual_fai()
     #FIXME: eventually, probably just write this to disk when constructed and return that way
+    import sys
+    sys.stderr.write(virtual_fai)
     return virtual_fai
 
 def create_virtual_fai():
     """implementation details of virtual fai construction
     """
     global genome_sizes
+    global genome_succession
     retval = ''
     offset = 0
     global virtual_fai_offsets
+    last_genome = None;
     with open('config.txt') as cfg:
         genomes = cfg.read().splitlines()
     for gnm in (genomes):
@@ -96,6 +140,10 @@ def create_virtual_fai():
         import sys
         sys.stderr.write('genome_size for ' + gnm + ' is ' + str(offset));
         genome_sizes[gnm] = offset
+        #for VirtualFile strategy
+        if last_genome is not None:
+            genome_succession[last_genome] = gnm
+        last_genome = gnm
     return retval
 
 def get_gzi():
@@ -158,7 +206,7 @@ def create_virtual_gzi():
         new_compressed_offset = start_compressed_offset + genome_sizes[gnm]
         import sys
         sys.stderr.write('offset for ' + gnm + ' is ' + str(start_compressed_offset) + '\n')
-        #the -1 was added to this after a commit that took place before a long hiatus from this code; not sure if it's a good thing but leaving for now while I try the MultiFile strategy
+        #the -1 was added to this after a commit that took place before a long hiatus from this code; not sure if it's a good thing but leaving for now while I try the VirtualFile strategy
         virtual_gzi_offsets[start_compressed_offset:new_compressed_offset-1] = {'genome':gnm, 'offset':start_compressed_offset}
         start_uncompressed_offset = new_uncompressed_offset
         start_compressed_offset = new_compressed_offset
@@ -168,7 +216,7 @@ def get_fa_slice(byterange):
     m = byterange_matcher.match(byterange)
     start = int(m.group(1))
     stop = int(m.group(2))
-    if virtual_gzi_offsets:
+    if len(virtual_gzi_offsets):
         import sys
         sys.stderr.write('interval overlaps='+str(len(virtual_gzi_offsets[start]))+'\n')
         d = virtual_gzi_offsets[start].pop().data
@@ -177,13 +225,7 @@ def get_fa_slice(byterange):
     gnm = d['genome']
     import sys
     sys.stderr.write('byte range from ' + str(start) + ' to ' + str(stop) + ' was matched to genome ' + gnm + ' with offset ' + str(d['offset']) + '\n')
-    #FIXME: this seems inefficient, but how can we redirect the client and tell them reset the range they had requested on the target? seems like it is not possible
-    if protocol_matcher.match(gnm):
-        f = urllib2.urlopen(urllib2.Request(gnm, headers = {'Range': 'bytes='+str(start-d['offset'])+'-'+str(stop-d['offset'])}))
-    else:
-        f = open(gnm)
-        sys.stderr.write('seeking to ' + str(start-d['offset']) + '\n')
-        f.seek(start-d['offset'])
+    f = VirtualFile(gnm,start-d['offset'],8192)
     r = make_response(send_file(f, 'X-application-fasta'), 206)
     r.headers['Content-Length'] = stop-start+1
     r.headers['Accept-Ranges'] = 'bytes'
