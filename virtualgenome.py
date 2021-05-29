6ac1465be98f772a4e3275da58406a2dccc5c1a3
adf-ncgr <adf@ncgr.org>
2021-05-28 21:33:27 -0600

    actually kind of works! I had an offset initialization wrong (starting at 1 instead of 0) and this was causing all kinds of subtle problems (only realized after comparing constructed fai to one from which it was being constructed...). Lots of debugging remains, no doubt but great to see it actually work in IGV with bgzipped remote files served from LIS!

--- virtualgenome.py
+++ virtualgenome.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, make_response
+from flask import Flask, request, send_file, make_response, Response
 import logging
 import urllib2
 from struct import pack, unpack
@@ -35,22 +35,54 @@ class VirtualFile(BufferedReader):
         self.current_f = self.open_local_or_remote_gnm(current_gnm,first_seek,size)
         #super(VirtualFile, self).__init__(self.current_f)
 
+    def open(self):
+        import sys
+        sys.stderr.write("open for bizness time!")
+        self.current_f = self.open_local_or_remote_gnm(current_gnm,0,8192)
+
+    def close(self):
+        import sys
+        sys.stderr.write("closing time!")
+        #self.current_f.close()
+        #if not self.closed:
+            #super(VirtualFile,self).close()
+
+    def generate(self,size):
+        b=None
+        while True:
+            b=self.read(size)
+            if len(b) == 0:
+                break;
+            yield b
+
     def read(self,size):
         import sys
         sys.stderr.write('read called with ' + self.current_gnm + ' with size = ' + str(size) + '\n')
-        #current_f = self.open_local_or_remote_gnm(self.current_gnm,self.first_seek)
-        b = self.current_f.read(size)
+        #if self.current_f.closed:
+            #self.current_f = self.open_local_or_remote_gnm(self.current_gnm,0,size)
+        b = ''
+        try:
+            b = self.current_f.read(size)
+        except ValueError:
+            pass
         while len(b) < size:
+            sys.stderr.write('len(b) now = ' + str(len(b)) + "\n")
             self.current_f.close()
             if self.current_gnm in genome_succession:
                 self.current_gnm = genome_succession[self.current_gnm]
                 self.current_f = self.open_local_or_remote_gnm(self.current_gnm,0,size)
+                sys.stderr.write("CROSSED OVER!\n")
                 b += self.current_f.read(size-len(b))
             else:
                 break
+        #sys.stderr.write("bytes is " + b + "\n")
         return b
 
     def open_local_or_remote_gnm(self,gnm,seek_to,size):
+        import sys
+        if seek_to < 0:
+            seek_to = 0
+        sys.stderr.write("seek_to="+str(seek_to)+" size="+str(size)+"\n")
         if protocol_matcher.match(gnm):
             return urllib2.urlopen(urllib2.Request(gnm, headers = {'Range': 'bytes='+str(seek_to)+'-'+str(seek_to+size-1)}))
         else:
@@ -82,6 +114,7 @@ def satisfy_head_check():
 @app.route('/virtualgenome.fa.gz', methods=['GET'])
 def return_fa_slice():
     import sys
+    sys.stderr.write('return_fa_slice called\n')
     #FIXME: do this with logging, though probably not really even needed anymore
     for header in request.headers.keys():
         sys.stderr.write(header + ' ' + request.headers.get(header) + '\n')
@@ -91,6 +124,8 @@ def return_fa_slice():
         #FIXME: do this with logging, though probably not really even needed anymore
         sys.stderr.write('byterange='+str(byterange)+'\n')
         return get_fa_slice(str(byterange))
+    else:
+        return get_fa_slice(str('bytes=1-8192'))
 
 def get_fai():
     """constuct a virtual genome by aggregating all the individual genomic fais
@@ -143,7 +178,7 @@ def create_virtual_fai():
         virtual_fai_offsets[offset:new_offset] = {'genome':gnm, 'offset':offset}
         offset = new_offset
         import sys
-        sys.stderr.write('genome_size for ' + gnm + ' is ' + str(offset));
+        sys.stderr.write('genome_size for ' + gnm + ' is ' + str(offset) + "\n");
         genome_sizes[gnm] = offset
         #for VirtualFile strategy
         if last_genome is not None:
@@ -157,7 +192,7 @@ def get_gzi():
     """
     global virtual_gzi
     import sys
-    sys.stderr.write("get_gzi called")
+    sys.stderr.write("get_gzi called\n")
     if not genome_sizes:
         get_fai()
     if not virtual_gzi:
@@ -187,7 +222,7 @@ def create_virtual_gzi():
             # All values are stored as little-endian 64-bit unsigned integers.
             num_entries = unpack('<Q',thisfile.read(8))[0]
             import sys
-            sys.stderr.write(gnm + " had num_entries = " + str(num_entries))
+            sys.stderr.write(gnm + " had num_entries = " + str(num_entries) + "\n")
             total_entries += num_entries
             i = 0
             while i < num_entries:
@@ -217,10 +252,14 @@ def create_virtual_gzi():
         #not sure using genome_size makes sense for compressed, but can't think of anything better
         #FIXME: follow IGV logic in determining end range which is some function of a fixed buffer size?
         new_compressed_offset = start_compressed_offset + genome_sizes[gnm]
+        #strangely, subbing this for the above caused the sequences retrieved to be wrong
+        #new_compressed_offset += GZI_BLOCK_SIZE
         import sys
         sys.stderr.write('offset for ' + gnm + ' is ' + str(start_compressed_offset) + '\n')
         #the -1 was added to this after a commit that took place before a long hiatus from this code; not sure if it's a good thing but leaving for now while I try the VirtualFile strategy
-        virtual_gzi_offsets[start_compressed_offset:new_compressed_offset-1] = {'genome':gnm, 'offset':start_compressed_offset}
+        #AFAICT does not seem to matter
+        #virtual_gzi_offsets[start_compressed_offset:new_compressed_offset-1] = {'genome':gnm, 'offset':start_compressed_offset}
+        virtual_gzi_offsets[start_compressed_offset:new_compressed_offset] = {'genome':gnm, 'offset':start_compressed_offset}
         start_uncompressed_offset = new_uncompressed_offset
         start_compressed_offset = new_compressed_offset
     return pack('<Q',total_entries)+retval
@@ -229,17 +268,21 @@ def get_fa_slice(byterange):
     m = byterange_matcher.match(byterange)
     start = int(m.group(1))
     stop = int(m.group(2))
+    import sys
     if len(virtual_gzi_offsets):
-        import sys
         sys.stderr.write('interval overlaps='+str(len(virtual_gzi_offsets[start:stop]))+'\n')
-        d = virtual_gzi_offsets[start:stop].pop().data
+        d = sorted(virtual_gzi_offsets[start:stop])[0].data
     else:
-        d = virtual_fai_offsets[start:stop].pop().data
+        sys.stderr.write('interval overlaps='+str(len(virtual_fai_offsets[start:stop]))+'\n')
+        d = sorted(virtual_fai_offsets[start:stop])[0].data
     gnm = d['genome']
     import sys
     sys.stderr.write('byte range from ' + str(start) + ' to ' + str(stop) + ' was matched to genome ' + gnm + ' with offset ' + str(d['offset']) + '\n')
-    f = VirtualFile(gnm,start-d['offset'],65536)
-    r = make_response(send_file(f, 'X-application-fasta'), 206)
+    #f = VirtualFile(gnm,start-d['offset']-1,65536)
+    f = VirtualFile(gnm,start-d['offset'],stop-start+1)
+    #r = make_response(send_file(f, 'X-application-fasta'), 206)
+    r = Response(f.generate(8192), mimetype='X-application-fasta')
+    r.status_code=206
     r.headers['Content-Length'] = stop-start+1
     r.headers['Accept-Ranges'] = 'bytes'
     return r
